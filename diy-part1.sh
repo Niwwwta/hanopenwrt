e09e05b94545d4921df4141256986c5bd5037d51
Niwwwta <129470731+Niwwwta@users.noreply.github.com>
2023-04-09 20:31:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/kenzok8/small-package package/small-package' >>feeds.conf.default
+echo 'src-git passwall https://github.com/kenzok8/small-package package/small-package' >>feeds.conf.default
